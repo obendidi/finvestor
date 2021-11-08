a5d82406f528739597566d35f68c8ac36df0d172
Ouail Bendidi <ouail.bendidi@gmail.com>
2021-11-08 16:05:12 +0100

    Update tests, and add fakse etoro statement data

--- tests/etoro/conftest.py
+++ tests/etoro/conftest.py
@@ -1,158 +1,41 @@
+import os
 from typing import Dict
 
 import pandas as pd
 import pytest
 
 
-@pytest.fixture
-def etoro_account_summary_df() -> pd.DataFrame:
-    raw_data = {
-        "Details": {
-            0: "Name",
-            1: "Name",
-            2: "Username",
-            3: "Currency",
-            4: "Date Created",
-            5: "Start Date",
-            6: "End Date",
-            7: None,
-            8: "Account Summary (USD)",
-            9: "Beginning Realized Equity",
-            10: "Deposits",
-            11: "Refunds",
-            12: "Credits",
-            13: "Adjustments",
-            14: "Profit or Loss (Closed positions only)",
-            15: "Rollover Fees",
-            16: "Withdrawals",
-            17: "Withdrawal Fees",
-            18: "Ending Realized Equity",
-            19: None,
-            20: "Unrealized Account Summary*",
-            21: "Beginning Unrealized Equity",
-            22: "Ending Unrealized Equity",
-            23: "*Unrealized Equity value is updated once a day",
-            24: None,
-            25: "eToro (Euro....",
-            26: None,
-            27: None,
-            28: "Notes:   This report data ......... ",
-        },
-        "Unnamed: 1": {
-            0: "Totals",
-            1: "John Doe",
-            2: "apes_together_strong",
-            3: "USD",
-            4: "01/01/1999 10:28:13",
-            5: "01/01/1998 00:00:00",
-            6: "01/01/1999 23:59:59",
-            7: None,
-            8: "Totals",
-            9: "0.00",
-            10: "123456.789 ",
-            11: "0.00",
-            12: "0.00",
-            13: "9.87",
-            14: "987654.321 ",
-            15: "1.17",
-            16: "10000.00",
-            17: "30.00",
-            18: "1101079.94 ",
-            19: None,
-            20: "Totals",
-            21: "0.00",
-            22: "1103079.94 ",
-            23: None,
-            24: None,
-            25: None,
-            26: None,
-            27: None,
-            28: None,
-        },
-    }
-    return pd.DataFrame(raw_data)
+@pytest.fixture(scope="session")
+def etoro_account_statement() -> Dict[str, pd.DataFrame]:
+    fake_etoro_data_path = os.path.join(
+        os.path.dirname(__file__), "__data__/fake-etoro-account-statement.xlsx"
+    )
+    return pd.read_excel(fake_etoro_data_path, sheet_name=None)
 
 
 @pytest.fixture
-def etoro_financial_summary_df() -> pd.DataFrame:
-    raw_data = {
-        "Name": {
-            0: "CFDs (Profit or Loss)",
-            1: "Crypto (Profit or Loss)",
-            2: "Stocks (Profit or Loss)",
-            3: "ETFs (Profit or Loss)",
-            4: "Stock Dividends (Profit)",
-            5: "CFD Dividends (Profit or Loss)",
-            6: "Income from Refunds",
-            7: "Commissions (spread) on CFDs",
-            8: "Commissions (spread) on Crypto",
-            9: "Commissions (spread) on Stocks",
-            10: "Commissions (spread) on ETFs",
-            11: "Fees",
-        },
-        "Amount\nin USD": {
-            0: 7654.0,
-            1: 8.8,
-            2: 98000.321,
-            3: 0.0,
-            4: 6.49,
-            5: 3.45,
-            6: 2.6,
-            7: 60.87,
-            8: 2.01,
-            9: 0.04,
-            10: 0.0,
-            11: 8.77,
-        },
-        "Tax\nRate": {
-            0: 0.0,
-            1: 0.0,
-            2: 0.0,
-            3: 0.0,
-            4: 0.0,
-            5: 0.0,
-            6: 0.0,
-            7: 0.0,
-            8: 0.0,
-            9: 0.0,
-            10: 0.0,
-            11: 0.0,
-        },
-    }
-    return pd.DataFrame(raw_data)
+def etoro_account_summary_df(
+    etoro_account_statement: Dict[str, pd.DataFrame]
+) -> pd.DataFrame:
+    return etoro_account_statement["Account Summary"]
 
 
 @pytest.fixture
-def etoro_closed_positions_df() -> pd.DataFrame:
-    raw_data = {
-        "Position ID": {0: 1, 1: 2},
-        "Action": {0: "Buy Awesome-stock Inc.", 1: "Sell Bad company"},
-        "Amount": {0: 1500.0, 1: 750.00},
-        "Units": {0: 500.12, 1: 6.52146},
-        "Open Date": {0: "01/01/2000 09:30:07", 1: "01/01/2000 10:10:15"},
-        "Close Date": {0: "12/06/2000 16:33:45", 1: "07/02/2002 15:12:15"},
-        "Leverage": {0: 1, 1: 5},
-        "Spread": {0: 0.0, 1: 10.43},
-        "Profit": {0: 512.65, 1: -654.21},
-        "Open Rate": {0: 2.99, 1: 115},
-        "Close Rate": {0: 4.02, 1: 215.32},
-        "Take profit rate": {0: 10, 1: 100},
-        "Stop lose rate": {0: 1, 1: 300},
-        "Rollover Fees and Dividends": {0: 0.0, 1: -2.03},
-        "Copied From": {0: "-", 1: "-"},
-        "Type": {0: "Real", 1: "CFD"},
-        "ISIN": {0: "US215621452", 1: "CH015236589"},
-        "Notes": {0: "", 1: ""},
-    }
-    return pd.DataFrame(raw_data)
+def etoro_financial_summary_df(
+    etoro_account_statement: Dict[str, pd.DataFrame]
+) -> pd.DataFrame:
+    return etoro_account_statement["Financial Summary"]
 
 
 @pytest.fixture
-def etoro_account_statement_sheets(
-    etoro_account_summary_df: pd.DataFrame,
-    etoro_financial_summary_df: pd.DataFrame,
-) -> Dict[str, pd.DataFrame]:
-    return {
-        "Account Summary": etoro_account_summary_df,
-        "Financial Summary": etoro_financial_summary_df,
-    }
+def etoro_closed_positions_df(
+    etoro_account_statement: Dict[str, pd.DataFrame]
+) -> pd.DataFrame:
+    return etoro_account_statement["Closed Positions"]
+
+
+@pytest.fixture
+def etoro_account_activity_df(
+    etoro_account_statement: Dict[str, pd.DataFrame]
+) -> pd.DataFrame:
+    return etoro_account_statement["Account Activity"]
--- tests/etoro/test_parsers.py
+++ tests/etoro/test_parsers.py
@@ -1,7 +1,8 @@
-from finvestor.etoro.io import (
+from finvestor.etoro.parsers import (
     parse_account_summary,
     parse_financial_summary,
     pre_process_closed_positions_df,
+    pre_process_account_activity_df,
 )
 from finvestor.etoro.schemas import EtoroAccountSummary, EtoroFinancialSummary
 
@@ -21,3 +22,13 @@ def test_parse_financial_summary(snapshot, etoro_financial_summary_df):
 def test_pre_process_closed_positions_df(snapshot, etoro_closed_positions_df):
     df = pre_process_closed_positions_df(etoro_closed_positions_df)
     assert df.to_dict() == snapshot
+
+
+def test_pre_process_account_activity_df(snapshot, etoro_account_activity_df):
+    fees_df, deposits_df, open_df, closed_df = pre_process_account_activity_df(
+        etoro_account_activity_df
+    )
+    assert fees_df.to_dict() == snapshot(name="fees")
+    assert deposits_df.to_dict() == snapshot(name="deposits")
+    assert open_df.to_dict() == snapshot(name="open_positions")
+    assert closed_df.to_dict() == snapshot(name="closed_positions")
